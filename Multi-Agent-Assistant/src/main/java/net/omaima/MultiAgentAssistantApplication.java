c8d4fb06f055115818afa22a565d69410ce9ab21
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-04-25 22:59:15 +0100

    fixinng ...

--- Multi-Agent-Assistant/src/main/java/net/omaima/MultiAgentAssistantApplication.java
+++ Multi-Agent-Assistant/src/main/java/net/omaima/MultiAgentAssistantApplication.java
@@ -1,4 +1,4 @@
-package net.omaima.multiagentassistant;
+package net.omaima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
